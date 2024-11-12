bf4a84dc14980705b75904394eca007caf7bc498
Xictoss <147993401+Xictoss@users.noreply.github.com>
2024-11-12 11:39:30 +0100

    Finish playerController

--- Assets/Scripts/Core/Player/PlayerMovement.cs
+++ Assets/Scripts/Core/Player/PlayerMovement.cs
@@ -17,35 +17,48 @@ namespace TheLastWitness.Core.Player
         [SerializeField] private float sprintMultiplier = 2f;
         [SerializeField] private float acceleration = 16f;
         [SerializeField] private float deceleration = 32f;
-        
+        [Header("Physic")] 
+        [SerializeField] private float gravity;
         [Header("Crouch")]
         [SerializeField] private float crounchMultiplier = 0.5f;
         
         private bool isMoving;
         private Vector2 targetVelocity;
+        private float verticalVelocity;
         
         // ChannelKey crouchkey = ChannelKey.GetUniqueChannelKey(); (créer une clée unique pour les channels)
         private void Awake()
         {
             CharacterController = GetComponent<CharacterController>();
+            
         }
         private void FixedUpdate()
         {
+            verticalVelocity -= gravity * Time.deltaTime;
             
             Vector3 forward = Vector3.ProjectOnPlane(GameCamera.Forward, transform.up).normalized;
             Vector3 right = Vector3.Cross(transform.up, forward).normalized;
-            Vector3 velocity = forward * targetVelocity.y + right * targetVelocity.x;
+            Vector3 velocity = forward * targetVelocity.y + right * targetVelocity.x + Vector3.up * verticalVelocity;
 
             if (IsCrouching)
             {
                 velocity *= crounchMultiplier;
             }
             
-            bool isGrounded = CharacterController.SimpleMove(velocity * SPEED_MULTIPLIER);
-            if (!isGrounded && IsCrouching)
+            CollisionFlags collisionFlags = CharacterController.Move(velocity * SPEED_MULTIPLIER * Time.deltaTime);
+            Debug.Log(collisionFlags);
+
+            bool isGrounded = (collisionFlags & CollisionFlags.Below) != 0;
+            
+            if (isGrounded)
+            {
+                verticalVelocity = 0;
+            }
+            if (!isGrounded)
             {
                 IsCrouching = false;
             }
+
         }
         
         public void OnMoveInput(InputAction.CallbackContext context)
